1c97428dcfd1dd876f824940a243d581478d9a55
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2021-08-28 11:00:00 +0200

    Upgrade to apollo-server-micro@3

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,31 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
 import SpotifyWebApi from 'spotify-web-api-node';
 
 import typeDefs from '../../resolvers/schema';
 import resolvers from '../../resolvers';
 
-const apolloServer = new ApolloServer({
-    context: async () => {
-        const client = new SpotifyWebApi({
-            clientId: process.env.SPOTIFY_ID,
-            clientSecret: process.env.SPOTIFY_SECRET,
-        });
+const setupApolloServer = async () => {
+    const apolloServer = new ApolloServer({
+        context: async () => {
+            const client = new SpotifyWebApi({
+                clientId: process.env.SPOTIFY_ID,
+                clientSecret: process.env.SPOTIFY_SECRET,
+            });
 
-        try {
-            const { body } = await client.clientCredentialsGrant();
+            try {
+                const { body } = await client.clientCredentialsGrant();
 
-            client.setAccessToken(body.access_token);
-        } catch (e) {
-            console.error(`Could not retrieve an access token: ${e}`);
-        }
+                client.setAccessToken(body.access_token);
+            } catch (e) {
+                console.error(`Could not retrieve an access token: ${e}`);
+            }
 
-        return {
-            client,
-        };
-    },
-    resolvers,
-    typeDefs,
-});
+            return {
+                client,
+            };
+        },
+        resolvers,
+        typeDefs,
+    });
+
+    await apolloServer.start();
+
+    return apolloServer;
+};
 
 export const config = {
     api: {
@@ -33,6 +40,11 @@ export const config = {
     },
 };
 
-export default apolloServer.createHandler({
-    path: '/api/graphql',
-});
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const apolloServer = await setupApolloServer();
+    const apolloHandler = apolloServer.createHandler({
+        path: '/api/graphql',
+    });
+
+    return await apolloHandler(req, res);
+};
